60dfaffa85329e37c342cdfc373b79d18275a714
Nazar Makivchuk <nazar.makivchuk@gmail.com>
2024-10-25 21:11:30 +0300

    create index

--- task.sql
+++ task.sql
@@ -1,2 +1,6 @@
 # Write your SQL code for the database creation here. Good luck! 
-USE ShopDB; 
+USE ShopDB;
+
+CREATE INDEX idx_customers_email ON Customers (Email);
+
+CREATE INDEX idx_products_name ON Products (Name);
